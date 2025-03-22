e62d43a4713365f469335abf604b0847731805b5
yume-baku <masato.umeno.818@gmail.com>
2025-03-22 20:15:43 +0900

    Swiperの挙動調整とコードの整理。課題提出準備完了

--- js/Swiper.js
+++ js/Swiper.js
@@ -1,6 +1,30 @@
 // スライダーSwiper.jsを使用するため
 // 【参考】https://kiomiru.co.jp/blog/coding/swiper-js/
 // 【参考(公式)】https://swiperjs.com/get-started
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs';
 
-const swiper = new Swiper(...)
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  spaceBetween: 0,
+  speed: 3000,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  autoplay: {
+    delay: 3000,
+  },
+  breakpoints: {
+    // bootstrapのブレイクポイント Medium: 768pxに合わせた
+    768: {
+      slidesPerView: 2,
+    },
+  },
+});
