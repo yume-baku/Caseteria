c8a52913bdaf8e8dfdd8d5f926006aebc2782aeb
yume-baku <masato.umeno.818@gmail.com>
2025-03-22 18:40:40 +0900

    swiper.jsをざっくり導入。動くが細かい調整ができていない

---
+++ js/Swiper.js
@@ -0,0 +1,6 @@
+// スライダーSwiper.jsを使用するため
+// 【参考】https://kiomiru.co.jp/blog/coding/swiper-js/
+// 【参考(公式)】https://swiperjs.com/get-started
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+
+const swiper = new Swiper(...)
